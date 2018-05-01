a1a69c424bb3e7af86d7398fefbd3dd0b029c37d
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-01 11:39:24 -0400

    Initial commit

---
+++ NewWalkingManTivaR/NewWalkingManForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace NewWalkingManTivaR
+{
+    partial class frmNewWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(280, 262);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(226, 28);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(199, 13);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Click on a picture to make the man walk.";
+            // 
+            // frmNewWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 552);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNewWalkingMan";
+            this.Text = "New Walking Man By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ NewWalkingManTivaR/NewWalkingManForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewWalkingManTivaR
+{
+    public partial class frmNewWalkingMan : Form
+    {
+        public frmNewWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePictureBoxes()
+        {
+            // Generate each of the picture boxes on the form
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+            GeneratePictureBox(61, 358);
+            GeneratePictureBox(464, 358);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Reveal the instructions
+            this.lblInstructions.Show();
+
+            // Generate the picture boxes with their event listeners
+            GeneratePictureBoxes();
+        }
+
+        private void GeneratePictureBox(int x, int y)
+        {
+            // Dynamically generate a new picture box and a new point at the given location (x, y)
+            PictureBox tmpPicMan = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            // Set location of the picture box
+            tmpPicMan.Location = pntPic;
+
+            // Assign the image to the picture box
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            // Strech the image to the size of the picture box
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Make the picture box width and height to be the same as the image
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            // Add the event listener
+            tmpPicMan.Click += new System.EventHandler(PictureBox_Click);
+
+            // Add the picture box to the form
+            this.Controls.Add(tmpPicMan);
+
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            // Cast the sender object into a picture Box
+            PictureBox tmpPicMan = (PictureBox)sender;
+
+            // Write a message to the console to check which picture box was clicked
+            Console.WriteLine("Picture box (" + tmpPicMan.Location.X + ", " + tmpPicMan.Location.Y + ") was clicked.");
+            
+        }
+    }
+}
